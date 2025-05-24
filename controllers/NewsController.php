856c074d58048038a3d37e719815a33f424da265
Ann Muha <ann.muha1209@gmail.com>
2025-05-24 12:20:00 +0300

    1.Ініціалізація структури MVC-проєкту: контролери, ядро, представлення

---
+++ controllers/NewsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace controllers;
+
+use core\Template;
+
+class NewsController
+{
+    // news/add
+    public function actionAdd() {
+        $template = new Template('views/news/add.php');
+        return [
+            'Content' => $template->getHTML(),
+            'Title' => 'Додавання новини',
+        ];
+    }
+    // news/index
+    public function actionIndex() {
+        $template = new Template('views/news/index.php');
+        return [
+            'Content' => $template->getHTML(),
+            'Title' => 'Список новин',
+        ];
+    }
+    public function actionView($params) {
+        return [
+            'Content' => 'News View',
+            'Title' => 'Перегляд новини',
+        ];
+    }
+}
